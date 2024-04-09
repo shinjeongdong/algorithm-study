4298b95db6bd0b5f632d7905125b5e32f99f257b
shinjeongdong <151175922+shinjeongdong@users.noreply.github.com>
2024-04-09 15:48:43 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	char a;
+	
+	while ((a = getchar()) != EOF)
+	{
+		putchar(a);
+	}
+
+	
+
+
+
+	return 0;
+}
